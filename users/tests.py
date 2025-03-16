ef59f3da2c292552f29e2485066784ddcc096a6b
su2708 <ysy2708@gmail.com>
2025-03-16 14:48:27 +0900

    add: AuthTests class

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class AuthTests(APITestCase):
+    def test_signup(self):
+        response = self.client.post('/signup/', {
+            'username': 'testuser',
+            'password': 'testpassword',
+            'nickname': 'testnickname'
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['username'], 'testuser')
+
+    def test_login(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        response = self.client.post('/login/', {
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('token', response.data)
